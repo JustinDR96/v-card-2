32ab910c7b703bbed5a4860ce3cbdd3a4aceb82c
JustinDR96 <justinderoover@gmail.com>
2023-11-26 17:27:54 +0100

    add style + librairie atropos

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import "./assets/styles/main.scss";
 const App = () => {
   return (
     <div id="app">
-      <Atropos className="my-atropos" activeOffset={60} shadow={false}>
-        <img src="/images/bg.jpg" />
+      <Atropos className="my-atropos" activeOffset={40} shadowScale={0.5}>
+        <div className="atropos-inner">
+          <div className="atropos-content">
+            <h1>De Roover Justin</h1>
+          </div>
+          <img src="/images/bg.jpg" data-atropos-offset="-5" />
+        </div>
       </Atropos>
     </div>
   );
